2d5089047382e4b630bdeaa5b9471e9d68de14ed
Casey Henry <casey@sparktoro.com>
2020-08-24 13:05:13 -0700

    Update config

--- config.rb
+++ config.rb
@@ -15,8 +15,8 @@ set :markdown,
     renderer: UniqueHeadCounter
 
 # Assets
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
+set :css_dir, 'docs/stylesheets'
+set :js_dir, 'docs/javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
 
